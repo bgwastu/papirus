958646768c1e4d689909590b51bd9651b288cef8
Bagas Wastu <67826350+bagaswastu@users.noreply.github.com>
2022-04-30 16:15:29 +0700

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ import { Group } from '@mantine/core';
 
 interface Props {
   leading: React.ReactNode;
-  menu: [React.ReactNode];
+  menu: React.ReactNode;
 }
 
 export default function Navbar(props: Props) {
