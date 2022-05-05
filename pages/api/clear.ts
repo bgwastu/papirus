82d2edf190beb49f07b2735eb89610ef72b9d2e4
Bagas Wastu <67826350+bagaswastu@users.noreply.github.com>
2022-05-05 11:53:33 +0700

    Remove seed, add clear endpoint
    Clear endpoint is created for clearing documents.

--- pages/api/clear.ts
+++ pages/api/clear.ts
@@ -1,15 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import sdk from 'node-appwrite';
 import { Server } from '../../stores/global';
-import { faker } from '@faker-js/faker';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === 'GET') {
-    // admin appwrite
-
     // If production, then return error
     if (process.env.NODE_ENV !== 'development') {
       return res.status(500).json({
@@ -25,10 +22,6 @@ export default async function handler(
       .setKey(Server.key || '') // Your secret API key
       .setSelfSigned(); // Use only on dev mode with a self-signed SSL cert
 
-    // Get first user
-    const users = new sdk.Users(client);
-    const user = (await users.list()).users[0];
-
     // Add list of notes to the appwrite
     const db = new sdk.Database(client);
 
@@ -41,26 +34,6 @@ export default async function handler(
       await db.deleteDocument(Server.collectionID || '', id);
     }
 
-    // Add new ones
-    for (let i = 0; i < 50; i++) {
-      const title = faker.lorem.words();
-      const content = `
-        <h1>${title}</h1>
-        <p>${faker.lorem.paragraphs()}</p>
-        <pre><code class="language-javascript">console.log("Hello world");</code></pre>
-        <p>${faker.lorem.paragraphs()}</p>
-      `;
-      db.createDocument(
-        Server.collectionID || '',
-        'unique()',
-        {
-          content,
-          timestamp: Date.now(),
-        },
-        [`user:${user.$id}`],
-        [`user:${user.$id}`]
-      );
-    }
     return res.status(200).json({ message: 'success' });
   }
 
